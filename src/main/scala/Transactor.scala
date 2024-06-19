66d0d2a11c107c2f20aef6a6cc40ec5406db921d
Scott Reynolds <sdrreynolds+gh@gmail.com>
2024-06-19 14:27:34 -0700

    Move to a Timer approach for canceling txns
    Why:
    As it moves to persistence layer, need to remove the thread state that
    the previous implementation leveraged

--- src/main/scala/Transactor.scala
+++ src/main/scala/Transactor.scala
@@ -13,6 +13,8 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.Duration
 import scala.concurrent.duration.FiniteDuration
 import org.apache.pekko.actor.Cancellable
+import scala.collection.immutable.Queue
+import java.time.Instant
 
 object Transactor {
   sealed trait Command[T]
@@ -22,20 +24,25 @@ object Transactor {
   case class FailedTransaction[T](transactionId: UUID, identifier: String, jsonObject: T) extends Command[T]
   private case class StartTransaction[T](replyTo: ActorRef[Response[T]], uuid: UUID, identifier: String, jsonObject: T) extends Command[T]
   private case class StartEmpty[T](replyTo: ActorRef[Response[T]]) extends Command[T]
+  private case class CleanTimedoutTransactions[T]() extends Command[T]
 
 
   sealed trait Response[T]
-  case class Transaction[T](id: String, identifier: String, jsonObject: T) extends Response[T]
+  case class Transaction[T](id: String, identifier: String, jsonObject: T, expires: Instant) extends Response[T]
   case class Empty[T]() extends Response[T]
 
   def apply[T](transactionDuration: FiniteDuration = FiniteDuration(10, "seconds")): Behavior[Command[T]] = {
     Behaviors.setup {context =>
-      val queue = context.spawnAnonymous(QueueOfQueues[T]("abc"))
-      transactorBehavior(Map.empty, queue, transactionDuration)
+      Behaviors.withTimers(factory => {
+        val queue = context.spawnAnonymous(QueueOfQueues[T]("abc"))
+        factory.startTimerWithFixedDelay(CleanTimedoutTransactions(), transactionDuration / 4)
+
+        transactorBehavior(Map.empty, queue, transactionDuration)
+      })
     }
   }
 
-  private def transactorBehavior[T](activeTransactions: Map[UUID, Cancellable], queue: ActorRef[QueueOfQueues.Command[T]], transactionDuration: FiniteDuration): Behavior[Command[T]] = {
+  private def transactorBehavior[T](activeTransactions: Map[UUID, Transaction[T]], queue: ActorRef[QueueOfQueues.Command[T]], transactionDuration: FiniteDuration): Behavior[Command[T]] = {
     Behaviors.receive((context, message) => {
       message match {
         case Enqueue(identifier, jsonObject) => {
@@ -54,28 +61,31 @@ object Transactor {
           Behaviors.same
         }
         case FailedTransaction(uuid, identifier, jsonObject) => {
-          // @TODO: this is a problem, we don't know the phone number
           queue ! QueueOfQueues.Enqueue(identifier, jsonObject)
-
-          // Canceling a already fired Cancelable is ok
-          activeTransactions.get(uuid).map(_.cancel())
           transactorBehavior(activeTransactions - uuid, queue, transactionDuration)
         }
         case CompleteTransaction(uuid) => {
-          activeTransactions.get(uuid).map(_.cancel())
           transactorBehavior(activeTransactions - uuid, queue, transactionDuration)
         }
 
         case StartTransaction(replyTo, uuid, identifier, jsonObject) => {
-          replyTo ! Transaction(uuid.toString(), identifier, jsonObject)
+          val txn = Transaction(uuid.toString(), identifier, jsonObject, Instant.now().plusMillis(transactionDuration.toMillis))
+          replyTo ! txn
 
-          transactorBehavior(activeTransactions +
-            (uuid -> context.scheduleOnce(transactionDuration, context.self, FailedTransaction(uuid, identifier, jsonObject))),
-            queue, transactionDuration)
+          transactorBehavior(activeTransactions + (uuid -> txn), queue, transactionDuration)
         }
         case StartEmpty(replyTo) => {
           replyTo ! Empty()
           Behaviors.same
+        }
+        case CleanTimedoutTransactions() => {
+          val current = Instant.now()
+          val expiredTransactions = activeTransactions
+            .filter((id, txn) => txn.expires.isAfter(current))
+
+          expiredTransactions.values.foreach(txn => queue ! QueueOfQueues.Enqueue(txn.identifier, txn.jsonObject))
+
+          transactorBehavior(activeTransactions -- expiredTransactions.keys, queue, transactionDuration)
         }
       }
     })
